e4f53917aee8e0f5390dcdde935e1764b6d9e178
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-07 04:08:55 +0545

    padding problem solved

--- src/shared/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation.js
@@ -2,41 +2,28 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import React from "react";
+import logo from "../../assets/img/logo/logo-2.png";
 
 const pages = ["Home", "Shop", "Collection", "Pages", "Contact Us"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 export default function Navigation() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box>1</Box>
-          <Box>2</Box>
-          <Box>3</Box>
+        <Box p={2}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              <img src={logo} alt="" />
+            </Box>
+            <Box>2</Box>
+            <Box>3</Box>
+          </Box>
         </Box>
       </Container>
     </AppBar>
