74688d190f5bffc55626a8f1d1ad04c858c8ae6d
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-07 18:16:43 +0545

    container added

--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -1,7 +1,25 @@
+import { Container } from "@mui/material";
+import Box from "@mui/material/Box";
+import logo from "../../assets/img/logo/logo.png";
 import React from "react";
-
 export default function Footer() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <Box >
+        <Container>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box>
+              <Box>
+                <img src={logo} alt="" />
+              </Box>
+            </Box>
+            <Box>2</Box>
+            <Box>3</Box>
+            <Box>4</Box>
+            <Box>5</Box>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
 }
