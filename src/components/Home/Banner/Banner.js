25d01ee59bf75166bcadd986748df4e20ca75e29
naimur <naimur202163@gmail.com>
2022-10-10 03:37:19 +0600

    working on slider

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-elastic-carousel";
 
+const items = [
+  { id: 1, title: "item #1" },
+  { id: 2, title: "item #2" },
+  { id: 3, title: "item #3" },
+  { id: 4, title: "item #4" },
+  { id: 5, title: "item #5" },
+];
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div>
+      <Carousel>
+        {items.map((item) => (
+          <div>
+            <div>1</div>
+            <div key={item.id}>{item.title}</div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
