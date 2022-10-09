04b6754fa97db7cfaad1256ec92b01f121821095
naimur <naimur202163@gmail.com>
2022-10-09 13:54:18 +0600

    working on nav

--- src/shared/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navigation() {
+  const [nav, setnav] = useState(false);
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-green-600">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Hello world!
+      </h1>
     </div>
   );
 }
