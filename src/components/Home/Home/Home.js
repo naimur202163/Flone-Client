5351b88cad5ac0e9cdf9e17a556b6c993214cbb9
naimur <naimur202163@gmail.com>
2022-10-10 02:57:01 +0600

    working with banner

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+
+export default function Homes() {
+  return (
+    <div className="container mx-auto">
+      <Banner />
+    </div>
+  );
+}
