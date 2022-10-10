a48e82df6ce541cdb0530658a017e1c905c17cc4
naimur <naimur202163@gmail.com>
2022-10-10 15:06:30 +0600

    working on cart page

--- src/components/Common/Cart/Cart.js
+++ src/components/Common/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function Cart() {
   return (
@@ -7,13 +8,11 @@ export default function Cart() {
         <h2 className="text-center text-2xl text-gray">Home/Cart</h2>
       </div>
       <div className="bg-white ">
-        <div className="flex justify-center align-center">
+        <div className="flex justify-center align-center mx-auto">
           <div>
-            <div></div>
-            <div>
-              <h2> No items found in cart</h2>{" "}
-              <button className="">shop now</button>
-            </div>
+            <AiOutlineShoppingCart className="text-center mx-auto" />
+            <h2 className="text-center mx-auto"> No items found in cart</h2>{" "}
+            <button className="text-center mx-auto">shop now</button>
           </div>
         </div>
       </div>
