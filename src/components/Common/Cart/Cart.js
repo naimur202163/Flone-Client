378ab95e96a5f0e494b48db4b35fc7329aee2179
naimur <naimur202163@gmail.com>
2022-10-10 14:33:25 +0600

    working with cart section

---
+++ src/components/Common/Cart/Cart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Cart() {
+  return (
+    <div className="bg-white">
+      <div className="bg-light">
+        <h2 className="text-center text-2xl text-gray">Home/Cart</h2>
+      </div>
+      <div className="bg-white ">
+
+            <div>
+            <div></div>
+            <div>   </div>
+            </div>
+
+      </div>
+    </div>
+  );
+}
