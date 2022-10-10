65eb8bc0d03151587deb296cba56ee1a4a0d5eed
naimur <naimur202163@gmail.com>
2022-10-10 14:38:26 +0600

    working with cart

--- src/components/Common/Cart/Cart.js
+++ src/components/Common/Cart/Cart.js
@@ -7,12 +7,15 @@ export default function Cart() {
         <h2 className="text-center text-2xl text-gray">Home/Cart</h2>
       </div>
       <div className="bg-white ">
-
-            <div>
+        <div className="flex justify-center align-center">
+          <div>
             <div></div>
-            <div>   </div>
+            <div>
+              <h2> No items found in cart</h2>{" "}
+              <button className="">shop now</button>
             </div>
-
+          </div>
+        </div>
       </div>
     </div>
   );
