27231e151bb55a3060944290edda6298ce61a0c4
naimur <naimur202163@gmail.com>
2022-10-10 15:22:54 +0600

    checkout page compeleted

--- src/components/Common/Checkout/Checkout.js
+++ src/components/Common/Checkout/Checkout.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { IoMedkitOutline } from "react-icons/io5";
 
 export default function Checkout() {
   return (
     <div className="bg-white">
       <div className="bg-light">
-        <h2 className="text-center text-2xl text-gray">HOME / CHECKOUT</h2>
+        <h2 className="text-center text-2xl text-gray">HOME /CHECKOUT</h2>
       </div>
       <div className="bg-white ">
         <div className="flex justify-center align-center mx-auto">
           <div>
-            <AiOutlineShoppingCart className="text-center mx-auto text-[10rem] p-5" />
+            <IoMedkitOutline className="text-center mx-auto text-[10rem] p-5" />
             <h2 className="text-center mx-auto text-2xl">
               {" "}
               No items found in cart
