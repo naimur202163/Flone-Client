c2daf6897d87dc1f8d65527d091ec10dea1724c8
naimur <naimur202163@gmail.com>
2022-10-10 20:33:24 +0600

    all routes added

--- src/components/Common/MyAccount/MyAccount.js
+++ src/components/Common/MyAccount/MyAccount.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function MyAccount() {
   return (
-    <div>MyAccount</div>
-  )
+    <div className="bg-white">
+      <div className="bg-light">
+        <h2 className="text-center text-2xl text-gray">HOME   /   MY ACCOUNT</h2>
+      </div>
+      <div className="bg-white "></div>
+    </div>
+  );
 }
