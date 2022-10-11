61612b1878ca866b4f2492cd82e973e095fe5e3f
ridoy113 <almamounhridoy113@gmail.com>
2022-10-11 10:41:21 +0600

    about page header done

--- src/components/About/About/About.js
+++ src/components/About/About/About.js
@@ -3,8 +3,13 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      About
-      <h1>Hello world</h1>
+
+      <div className='flex justify-center py-[2rem] bg-zinc-100 font-semibold'>
+        <p className='text-gray cursor-pointer'>HOME</p>
+        <span className='px-[0.7rem]'>/</span>
+        <p>ABOUT US</p>
+      </div>
+      
     </div>
   )
 }
