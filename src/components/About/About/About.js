79c06f74dfb5460f39680d8208dee42c8a5db359
ridoy113 <almamounhridoy113@gmail.com>
2022-10-12 15:24:34 +0600

    about team member section almost complite

--- src/components/About/About/About.js
+++ src/components/About/About/About.js
@@ -3,6 +3,7 @@ import { BsArrowRight } from 'react-icons/bs'
 import { FiShoppingBag } from 'react-icons/fi'
 import { RiCupLine, RiLightbulbLine } from 'react-icons/ri'
 import { BsEmojiSmile } from 'react-icons/bs'
+import CountUp from 'react-countup';
 
 
 
@@ -30,8 +31,8 @@ export default function About() {
 
 
 
-      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-x-[2rem]
-      gap-y-[2rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[2rem]
+      gap-y-[1rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem]'>
 
         <div className="bg-gradient-to-tr overflow-hidden relative h-[14rem]">
           <img
@@ -89,8 +90,8 @@ export default function About() {
 
 
 
-      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-x-[2rem]
-      gap-y-[2rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem] my-[4.8rem]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[2rem]
+      gap-y-[1rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem] my-[4.8rem]'>
 
         <div>
           <h1 className='text-2xl font-medium text-[#403d3d] pb-[1.2rem]'>Our Vision</h1>
@@ -109,34 +110,87 @@ export default function About() {
 
 
 
-      <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-[2rem]
+      gap-y-[1rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem] my-[4.8rem] bg-slate-100 text-center'>
 
-        <div>
-          <p><FiShoppingBag /></p>
-          <h1></h1>
-          <h4>Project Done</h4>
+        <div className='py-[5rem] mx-auto'>
+          <p className='text-5xl text-[#3a3838] pl-[3rem]'><FiShoppingBag /></p>
+          <h1 className='text-[#A749FF] text-4xl font-bold py-[1rem] tracking-wide'><CountUp start={10} end={360} /></h1>
+          <h4 className='text-[#222121] text-2xl font-medium'>Project Done</h4>
         </div>
 
-        <div>
-          <p><RiCupLine /></p>
-          <h1></h1>
-          <h4>Cups Of Coffee</h4>
+        <div className='py-[5rem] mx-auto'>
+          <p className='text-5xl text-[#3a3838] pl-[3.5rem]'><RiCupLine /></p>
+          <h1 className='text-[#A749FF] text-4xl font-bold py-[1rem] tracking-wide'><CountUp start={10} end={690} /></h1>
+          <h4 className='text-[#222121] text-2xl font-medium'>Cups Of Coffee</h4>
         </div>
 
-        <div>
-          <p><RiLightbulbLine /></p>
-          <h1></h1>
-          <h4>Branding</h4>
+        <div className='py-[5rem] mx-auto'>
+          <p className='text-5xl text-[#3a3838] pl-[2rem]'><RiLightbulbLine /></p>
+          <h1 className='text-[#A749FF] text-4xl font-bold py-[1rem] tracking-wide'><CountUp start={10} end={100} /></h1>
+          <h4 className='text-[#222121] text-2xl font-medium'>Branding</h4>
         </div>
 
-        <div>
-          <p><BsEmojiSmile /></p>
-          <h1></h1>
-          <h4>Happy Clients</h4>
+        <div className='py-[5rem] mx-auto'>
+          <p className='text-5xl text-[#3a3838] pl-[3rem]'><BsEmojiSmile /></p>
+          <h1 className='text-[#A749FF] text-4xl font-bold py-[1rem] tracking-wide'><CountUp start={10} end={420} /></h1>
+          <h4 className='text-[#222121] text-2xl font-medium'>Happy Clients</h4>
         </div>
 
       </div>
 
+
+
+
+      <div className='text-center px-[1.3rem] md:px-[3.5rem] lg:px-[20rem] 
+      py-[2rem] md:py-[4rem] lg:py-[2rem]'>
+        <h1 className='text-3xl font-semibold leading-loose'>Team Members</h1>
+        <div className='mx-auto'>
+          <p className='inline-block h-[2px] w-[5rem] bg-black my-2'></p>
+        </div>
+        <p className='leading-loose font-medium text-[#454444] tracking-wide'>Lorem ipsum dolor sit amet conse ctetu.</p>
+      </div>
+
+
+
+
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-[2rem]
+      gap-y-[1rem] px-[1.3rem] md:px-[3rem] lg:px-[5rem] my-[4.8rem] text-center'>
+
+        <div className='bg-slate-100'>
+          <img className='w-full' src="https://i.ibb.co/52ZjS4s/team-4.jpg" alt="" />
+          <div className='p-[1rem]'>
+            <h3 className='text-[1.3rem] font-semibold leading-loose'>Mr.Peter Pan</h3>
+            <p className='italic'>Developer</p>
+          </div>
+        </div>
+
+        <div className='bg-slate-100'>
+          <img className='w-full' src="https://i.ibb.co/qFzwVC3/team-3.jpg" alt="" />
+          <div className='p-[1rem]'>
+            <h3 className='text-[1.3rem] font-semibold leading-loose'>Mr.Mike Banding</h3>
+            <p className='italic'>Manager</p>
+          </div>
+        </div>
+
+        <div className='bg-slate-100'>
+          <img className='w-full' src="https://i.ibb.co/gyWRmCH/team-2.jpg" alt="" />
+          <div className='p-[1rem]'>
+            <h3 className='text-[1.3rem] font-semibold leading-loose'>Ms.Sophia</h3>
+            <p className='italic'>Designer</p>
+          </div>
+        </div>
+
+        <div className='bg-slate-100'>
+          <img className='w-full' src="https://i.ibb.co/sKDNk6z/team-1.jpg" alt="" />
+          <div className='p-[1rem]'>
+            <h3 className='text-[1.3rem] font-semibold leading-loose'>Mr.John Lee</h3>
+            <p className='italic'>Chairman</p>
+          </div>
+        </div>
+
+      </div>
 
 
 
